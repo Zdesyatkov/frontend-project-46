a0151f1e09e74f5699b9bc7c06cb9197329280dc
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-03-27 22:36:25 +0300

    step1,2

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .description('Compares two configuration files and shows a difference.');
+program.parse();
+
+  
