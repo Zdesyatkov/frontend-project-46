5e40a0698d971339ffb6070a18b43522b803cb71
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-03-31 11:16:36 +0300

    parser

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
-  .description('Compares two configuration files and shows a difference.');
-program.parse();
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+
+program.parse(process.argv);
 
   
