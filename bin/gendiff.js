3993128ce57210471265dc308750abe4f7d1a51c
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-04-02 19:49:05 +0300

    add step 3,4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,13 +3,14 @@ import { Command } from 'commander/esm.mjs';
 import gendiff from '../src/index.js';
 
 const program = new Command();
+
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    gendiff(filepath1, filepath2)
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format))
   })
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
+import compareData from './compareData.js';
+import format from './formatters/index.js';
 
 
 const getAbsolutPath = (filepath) => path.resolve(process.cwd(), filepath)
-const readFile = (filepath) => fs.readFileSync(getAbsolutPath(filepath))
+const readFile = (filepath) => fs.readFileSync(getAbsolutPath(filepath), 'utf-8')
 const getFormat = (filename) => filename.split('.')[1];
 
-const gendiff = (file1, file2) => {
+const gendiff = (file1, file2, formatName = 'stylish') => {
     const data1 = readFile(file1);
     const data2 = readFile(file2);
     const parsed1 = parse(data1, getFormat(file1));
     const parsed2 = parse(data2, getFormat(file2));
-    const data = (`${parsed1}, ${parsed2}`);
-    console.log (parsed1);
-    console.log (parsed2);
+    const data = compareData(parsed1, parsed2)
+    //console.log (parsed1);
+    //console.log (parsed2);
+    return format(data, formatName);
   };
   
   export default gendiff;
